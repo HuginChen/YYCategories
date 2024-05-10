bcb60f2e00403edcbd35d4518225f7318c3433b4
Hugin <>
2024-05-10 17:03:47 +0800

    ios 17闪退

--- YYCategories/UIKit/UIView+YYAdd.m
+++ YYCategories/UIKit/UIView+YYAdd.m
@@ -19,6 +19,9 @@ YYSYNTH_DUMMY_CLASS(UIView_YYAdd)
 @implementation UIView (YYAdd)
 
 - (UIImage *)snapshotImage {
+    if (self.bounds.size.width == 0 || self.bounds.size.height == 0) {
+        return [[UIImage alloc] init];
+    }
     UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0);
     [self.layer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *snap = UIGraphicsGetImageFromCurrentImageContext();
